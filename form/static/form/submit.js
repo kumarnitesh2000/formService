8a1ba4a948c4a52297c8cb41417fdcf488070e9d
Hardik Sharma <54437046+hardikshah197@users.noreply.github.com>
2020-08-26 16:14:48 +0530

    Updated JSON Skeleton
    Updated Submit.js along with latest changes of JSON skeleton containing values submission

--- form/static/form/submit.js
+++ form/static/form/submit.js
@@ -1,8 +1,49 @@
+var objJson = {
+    "form": {
+
+        "form_title":"Untitled Form",
+        "form_description":"Enter The Description"
+    },
+
+    "section":{
+        "section_title":[],
+        "section_description":[]
+    },
+
+    "field":{
+
+        "field_description":[],
+        "field_label":[],
+        "field_optgroup_type":[{
+            "Choices":[{"field_type": "dropdown", "options": ["male","female"]}]
+        },{
+            "NoChoices":["textfield","fileupload"]
+        }
+    ]
+
+    }
+    
+  }
 var but = document.getElementById('submit');
 //TODO
 let submitJson  = () =>{
 //For Testing Alert
-alert('Hello Please Contribute !');
-
+    var sec = document.getElementsByClassName("section").length;
+    
+    var fld = document.getElementsByClassName("field").length;
+    
+    for(var i =1; i<= sec; i++){
+        var p = document.getElementById("sec_title_"+i);
+        objJson.section.section_title.push(p.value);
+        objJson.section.section_description.push(document.getElementById("sec_des_"+i).value);
+    }
+    for(var j=1;j<= fld;j++){
+        var p= document.getElementById("description_"+j);
+        objJson.field.field_description.push(p.value);
+        objJson.field.field_label.push(document.getElementById("label_"+j).value);
+        //var t= document.getElementById('choice_'+j);
+        
+    }
+    console.log(objJson);
 }
 but.addEventListener('click',submitJson);
