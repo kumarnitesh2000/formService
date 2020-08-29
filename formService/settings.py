3c6361a84fe33dd7ff3afe8b35fe41620b61dca6
kumarnitesh2000 <kumarnitesh2000.nk@gmail.com>
2020-08-28 23:23:47 -1000

    test passed

--- formService/settings.py
+++ formService/settings.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'form.apps.FormConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -26,6 +27,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -34,6 +37,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000"
+]
+
 
 ROOT_URLCONF = 'formService.urls'
 
